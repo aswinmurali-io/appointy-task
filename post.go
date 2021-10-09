d5733d07cd1244eedfb2305c79909f700b124c1d
Aswin Murali <aswinmurali.co@gmail.com>
2021-10-09 21:41:51 +0530

    adding post will notify `user` collection to add reference of `post`

--- post.go
+++ post.go
@@ -3,7 +3,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -12,24 +14,49 @@ type Post struct {
 	Caption         string             `bson:"caption" json:"caption"`
 	ImageUrl        string             `bson:"imageurl" json:"imageurl"`
 	PostedTimestamp string             `bson:"postedtimestamp" json:"postedtimestamp"`
+	UserId          primitive.ObjectID `bson:"userid" json:"userid"`
 }
 
 type Posts []Post
 
 func (post Post) add() {
+	Collection := Database.Collection("users")
 	fmt.Println("INFO: Inserting user")
 	result, errorInInsert := Database.Collection("posts").InsertOne(
 		MongoContext, post,
 	)
-	fmt.Println(result)
-	fmt.Println(errorInInsert)
+
+	// Append the post ID into newuser
+	newuser := User{}
+	errorInDecode := Collection.FindOne(
+		MongoContext, bson.M{"_id": post.UserId},
+	).Decode(&newuser)
+	if errorInDecode != nil {
+		log.Println(errorInDecode)
+	}
+	newuser.Posts = append(newuser.Posts, post.Id)
+	log.Println(newuser.Posts)
+
+	// Update user
+	Collection.DeleteOne(MongoContext, bson.M{"_id": post.UserId})
+	Collection.InsertOne(MongoContext, newuser)
+
+	log.Println(result)
+	log.Println(errorInInsert)
 }
 
-func (user Post) remove() {
-	fmt.Println("INFO: Removing user")
-	result, errorInDelete := Database.Collection("posts").DeleteOne(
-		MongoContext, user,
-	)
-	fmt.Println(result)
-	fmt.Println(errorInDelete)
+func (post Post) get() Post {
+	Collection := Database.Collection("posts")
+	log.Printf("[INFO] Getting post from id %s.\n", post.Id)
+
+	newpost := Post{}
+	errorInDecode := Collection.FindOne(
+		MongoContext, bson.M{"_id": post.Id},
+	).Decode(&newpost)
+
+	if errorInDecode != nil {
+		log.Println(errorInDecode)
+	}
+
+	return newpost
 }
