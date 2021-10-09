a9b3f3863d10b1b4753a1c271dc98cac5c0a2da6
Aswin Murali <aswinmurali.co@gmail.com>
2021-10-09 20:09:01 +0530

    cleans code

--- userpage.go
+++ userpage.go
@@ -4,14 +4,16 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/mail"
 	"strings"
 
-	// "go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
+
+const RequestBodyErrorMsg = "Unable to read the request!"
 
 func userGetDetailsPage(response http.ResponseWriter, request *http.Request) {
 	_, errorInBody := ioutil.ReadAll(request.Body)
@@ -20,46 +22,57 @@ func userGetDetailsPage(response http.ResponseWriter, request *http.Request) {
 	if errorInBody == nil {
 		objectId, errorInHex := primitive.ObjectIDFromHex(idStringInHex)
 		if errorInHex != nil {
-			fmt.Println("ERROR: Unable to get object id from hex value.")
+			fmt.Println(response, "Unable to get object id from hex value.")
+			log.Printf("[ERROR] Unable to get object id from hex value %s.\n", idStringInHex)
 		}
-		userBson, errorInJson := User{Id: objectId}.get().DecodeBytes()
-		if errorInJson != nil {
-			fmt.Println("ERROR: Error in reading json from content")
+		userBson, errorInBson := User{Id: objectId}.get().DecodeBytes()
+		if errorInBson != nil {
+			fmt.Fprintf(response, "Error in reading json from content")
+			log.Printf("[ERROR] Error in reading json from content %s.\n", userBson)
 			return
 		}
-		fmt.Println(userBson)
+
+		// Output the response
 		var doc bson.Raw
 		bson.Unmarshal(userBson, &doc)
 		fmt.Fprintf(response, "%s", doc.String())
-		if errorInJson != nil {
-			fmt.Fprintf(response, "ERROR: Unable to unmarshal this json")
-			fmt.Println(errorInJson)
+		log.Println(userBson)
+
+		if errorInBson != nil {
+			fmt.Fprintf(response, "Unable to unmarshal this bson")
+			log.Printf("[ERROR] Unable to unmarshal this bson\n")
+			log.Println(errorInBson)
 			return
 		}
 	} else {
-		fmt.Println("ERROR: Error in reading request body.")
-		fmt.Println(errorInBody)
+		fmt.Fprintf(response, RequestBodyErrorMsg)
+		log.Println("[ERROR] Error in reading request body.")
+		log.Println(errorInBody)
 	}
 }
 
+// Create user using the POST method
 func userCreatePage(response http.ResponseWriter, request *http.Request) {
+	// Reading request body
 	body, errorInBody := ioutil.ReadAll(request.Body)
 	if errorInBody == nil {
+		// Json validation
 		keyVal := make(map[string]string)
 		errorInUnmarshal := json.Unmarshal(body, &keyVal)
 
-		// Json validation
 		if errorInUnmarshal == nil {
 			// Name validation
 			if keyVal["name"] == "" || keyVal["name"] == " " {
-				fmt.Fprintf(response, "ERROR: 'name' key cannot be empty!")
+				fmt.Fprintf(response, "Key `name` has empty value!")
+				log.Printf("[ERROR] 'name' key cannot be empty! %s.\n", keyVal)
 				return
 			}
 
 			// Email validation
 			_, errorInParsingMail := mail.ParseAddress(keyVal["email"])
 			if errorInParsingMail != nil {
-				fmt.Fprintf(response, "ERROR: Invalid email address!")
+				fmt.Fprintf(response, "Invalid email address!")
+				log.Printf("[ERROR] Invalid email address! %s.\n", keyVal["email"])
 				return
 			}
 
@@ -70,22 +83,25 @@ func userCreatePage(response http.ResponseWriter, request *http.Request) {
 				Password: keyVal["password"],
 			}
 			user.add()
+			fmt.Fprintf(response, "Added user %s", user.Id)
 
 			// print the user object they send as response
 			json.NewEncoder(response).Encode(user)
 		} else {
-			fmt.Println("ERROR: Unable to unmarshal json value.")
-			fmt.Println(errorInUnmarshal)
+			log.Printf("[ERROR] Unable to unmarshal json value %s.\n", body)
+			log.Println(errorInUnmarshal)
+			return
 		}
 	} else {
-		fmt.Println("ERROR: Error in reading request body.")
-		fmt.Println(errorInBody)
+		fmt.Fprintf(response, RequestBodyErrorMsg)
+		log.Printf("[ERROR] Error in reading request body %s.\n", request.Body)
+		log.Println(errorInBody)
 	}
-
 }
 
-// User route
+// User route handler which binds logic for POST and GET method.
 func userPage(response http.ResponseWriter, request *http.Request) {
+	log.Printf("[INFO] Request %s triggered", request.Method)
 	switch request.Method {
 	case "GET":
 		userGetDetailsPage(response, request)
